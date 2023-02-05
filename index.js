dbdd872eea4dec7f63cd41e7f9ea40d9cad7c4b5
Mahek Unnisa <mahekunnisa786@gmail.com>
2023-02-05 19:42:40 +0530

    Server

--- index.js
+++ index.js
@@ -1,19 +1,12 @@
 const express = require('express');
 const app = express()
 const PORT = process.env || 3000
+const path = require('path')
 app.get('/',(req,res) => {
     res.status(200);
     res.sendFile(path.join(__dirname, 'index.html'));
 })
-serverapp.use('/static', express.static(path.join(__dirname, 'static')));
+app.use('/static', express.static(path.join(__dirname, 'static')));
 app.use(PORT, ()=>{
     console.log(`Server is listening on ${PORT}`)
 })
-
-
-
-
-
-//Source: https://stackoverflow.com/questions/71451988
-
-
